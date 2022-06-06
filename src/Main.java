4700a1cf8f08a3688357dfb1af88f67fb711de80
hanawinchester <hana.rose.winchester@gmail.com>
2022-06-05 20:27:26 -0400

    Initial Commit

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ public class Main
     public static void main(String[] args)
     {
         double purchasePrice = 20.20; // price
-        double salesTax = purchasePrice * .05; //tax
+        double salesTax = purchasePrice * .05; // tax
         double Total = purchasePrice + salesTax; //total
         System.out.println("The sales tax for a purchase of $" + purchasePrice + " is $" + salesTax + ". Your total price is $" + Total + ".");
     }
